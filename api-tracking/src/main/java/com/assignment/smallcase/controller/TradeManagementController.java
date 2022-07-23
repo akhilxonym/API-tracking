f56a3e5f8696b10e20aa5ab961c2fe07442c2326
akhilnigam <akhilnigam@localhost>
2022-07-23 15:16:01 +0530

    created trade management controller and added quantity and price validation

--- api-tracking/src/main/java/com/assignment/smallcase/controller/TradeManagementController.java
+++ api-tracking/src/main/java/com/assignment/smallcase/controller/TradeManagementController.java
@@ -4,8 +4,15 @@ import java.lang.invoke.MethodHandles;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.assignment.smallcase.request.AddTradeRequest;
+import com.assignment.smallcase.response.AddTradeResponse;
+import com.assignment.smallcase.utility.QtyAndPriceValidator;
 
 @RestController
 @RequestMapping(value="/stock/order")
@@ -13,5 +20,8 @@ public class TradeManagementController {
 	
 	private static final Logger logger= LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	
-
+	@PostMapping
+	public ResponseEntity<AddTradeResponse> addTrade(@QtyAndPriceValidator @RequestBody AddTradeRequest addTradeRequest){
+		return null;
+	}
 }

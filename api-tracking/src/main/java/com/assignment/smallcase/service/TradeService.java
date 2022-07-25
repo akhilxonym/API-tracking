a0f9cc8d607fbf665622289ee4b48813d1f0f394
akhilnigam <akhilnigam@localhost>
2022-07-26 00:30:43 +0530

    added basic spring security and trade details controller

---
+++ api-tracking/src/main/java/com/assignment/smallcase/service/TradeService.java
@@ -0,0 +1,28 @@
+package com.assignment.smallcase.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.assignment.smallcase.model.Trade;
+import com.assignment.smallcase.repository.TradeRepository;
+
+@Service
+public class TradeService {
+	@Autowired
+	private TradeRepository tradeRepository;
+	
+	public ResponseEntity<List<Trade>> getAllTrades(){
+		return ResponseEntity.ok(tradeRepository.findAll());
+	}
+	
+	public ResponseEntity<Trade> getTradeById(String tradeId){
+		Optional<Trade> trade=tradeRepository.findById(tradeId);
+		if(trade.isEmpty())
+			return ResponseEntity.ok(null);
+		return ResponseEntity.ok(trade.get());
+	}
+}

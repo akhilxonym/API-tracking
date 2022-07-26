73117707db77403c69650def4a8b270e01f54f84
akhilnigam <akhilnigam@localhost>
2022-07-26 18:47:26 +0530

    added remove trade endpoint and images of test results

--- api-tracking/src/main/java/com/assignment/smallcase/controller/TradeManagementController.java
+++ api-tracking/src/main/java/com/assignment/smallcase/controller/TradeManagementController.java
@@ -8,8 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,17 +23,23 @@ import com.assignment.smallcase.service.TradeManagementService;
 import com.assignment.smallcase.utility.QtyAndPriceValidator;
 
 @RestController
-@RequestMapping(value="/stock/order")
+@RequestMapping(value="/stock/trade")
 public class TradeManagementController {
 	@Autowired
 	private TradeManagementService tradeManagementService;
 	
 	private static final Logger logger= LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	
-	@PostMapping
+	@PostMapping("/order")
 	public ResponseEntity<AddTradeResponse> addTrade(@RequestParam(required = false) String tradeId,@RequestBody @Valid AddTradeRequest addTradeRequest){
+		logger.info("calling add trade method");
 		return tradeManagementService.addTrade(tradeId,addTradeRequest);
 	}
 	
-	
+	@DeleteMapping
+	@RequestMapping("/remove")
+	public ResponseEntity<String> removeTrade(@RequestParam(required = true) String tradeId){
+		logger.info("calling remove trade method for"+tradeId);
+		return tradeManagementService.removeTrade(tradeId);
+	}
 }
